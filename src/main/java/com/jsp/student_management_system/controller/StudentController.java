5ba873b2b21e376b6133ef9e9bc9b0237316b93f
chandrashekar Gj <chandrashekargj27@gmail.com>
2025-05-12 13:31:40 +0530

    Fetch Completed

--- src/main/java/com/jsp/student_management_system/controller/StudentController.java
+++ src/main/java/com/jsp/student_management_system/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.jsp.student_management_system.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +22,25 @@ public class StudentController {
 		return "main.html";
 	}
 	
+	//Getting The Add.html Page in the Browser.
 	@GetMapping("/add")
 	public String loadAdd() {
 		return "add.html";
 	}
 	
+	//To Save the Data into the Database.
 	@PostMapping("/add")
 	public String add(@ModelAttribute Student student, Model model) {
 		studentRepository.save(student);
 		model.addAttribute("msg","Data added Succesfull");
 		return "main.html";	
 	}
-
+	
+	//Fetch the Student Details.
+	@GetMapping("/fetch")
+	public String loadFetch(Model model) {
+		List<Student> list=studentRepository.findAll();
+		model.addAttribute("list",list);
+		return "fetch.html";
+	}
 }
